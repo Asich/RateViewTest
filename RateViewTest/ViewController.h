38309d59a7dc696eab868c00dc90be271cc017d3
Asich <mustafinaskar@gmail.com>
2013-04-17 13:21:31 +0600

    Initial Commit

---
+++ RateViewTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RateViewTest
+//
+//  Created by Mustafin Askar on 17.04.13.
+//  Copyright (c) 2013 askar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
